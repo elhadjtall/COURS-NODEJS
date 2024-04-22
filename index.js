35e71e68cc101d37192a2601b02c7fb8667b70ca
unknown <elhadjoumartall10@gmail.com>
2024-04-22 12:19:33 +0200

    Cours nodejs

---
+++ index.js
@@ -0,0 +1,18 @@
+// on import  express et nodemon
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Ici on va se connecter à la base de donnée grêce Mongoose
+mongoose.connect('', {})
+.then(() => {
+    console.log("connecter à la BDD");
+})
+.catch((err) => {
+    console.log('Erreur de connexion', err)
+})
+// On va initialiser et demarerrer notre serveur
+let app = express();
+let port = 3000;
+app.listen(port, () => {
+    console.log("Server en ligne sur le port 3000");
+});
